dfc90c7f766e1c14ec559989876660b1c8ab30a3
Aijan-Abil <arstanlovaaijan@gmail.com>
2026-06-03 16:11:40 +0600

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './modules/product/product.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import databaseConfig from './config/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getTypeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [ProductModule,],
+  imports: [
+    ProductModule,
+    ConfigModule.forRoot({
+      isGlobal:true,
+      load: [databaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: getTypeOrmConfig, 
+    }),
+  ],
   controllers: [AppController,],
   providers: [AppService],
 })
